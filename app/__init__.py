8dfd242f7b926a3857de27632ffdea3bd13bd9a9
Clarissa Borges <clarissalimab08@gmail.com>
2020-05-23 20:11:56 -0300

    Adds user register route

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from pymongo import MongoClient
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -8,7 +9,9 @@ if app.config["ENV"] == "production":
 else:
     app.config.from_object("config.DevelopmentConfig")
 
-mongo = app.config["MONGO_URI"]
+flask_bcrypt = Bcrypt(app)
+
+mongo = MongoClient(app.config["MONGO_URI"])
 
 from .users import users
 app.register_blueprint(users, url_prefix="/api/users")
--- app/users/routes.py
+++ app/users/routes.py
@@ -1,8 +1,31 @@
 from app.users import users
 from flask import Flask, jsonify, request
+from app import mongo, flask_bcrypt
 
 @users.route("/ping", methods = ["GET"])
 def ping():
     return jsonify({"success" : True,
                     "response" : "pong!"
                     }), 200
+
+@users.route("/register", methods = ["POST"])
+def register():
+    users = mongo.db.users
+
+    existing_user = users.find_one({"username" : request.json["username"]})
+
+    if existing_user is None:
+        hashpass = flask_bcrypt.generate_password_hash(request.json["password"])
+
+        users.insert_one({
+                        "username" : request.json["username"],
+                        "password" : hashpass
+                    })
+        
+        return jsonify({"success"  : True,
+                        "response" : "User created!"
+                        }), 200
+    
+    return jsonify({"success"  : False,
+                    "response" : "User already exists!"
+                    }), 400
